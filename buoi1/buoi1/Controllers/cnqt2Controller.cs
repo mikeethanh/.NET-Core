25b1a568c1e4a7972514324dd9f20c31077a9b94
mikee thanh <mikeethanh04@gmail.com>
2024-08-19 20:13:35 +0700

    init

---
+++ buoi1/buoi1/Controllers/cnqt2Controller.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using buoi1.Models;
+namespace buoi1.Controllers
+{
+    public class cnqt2Controller : Controller
+    {
+        public IActionResult Index()
+        {
+            //de trao doi du lieu tu action ve view co 2 cach 
+            //c1: dung cac bo nho chia se nhu: view +bag, viewdata,appdata
+            //c2: dung model 
+            //vi su cach 1:
+            ViewBag.Name = "Nguyen van a";
+            ViewBag.Age = 22;
+            ViewBag.Score = 4.5;
+            //cach2:
+            user u = new user(1000, "nmt", "aafasf");
+            return View("abc",u);
+        }
+        public IActionResult MyAction() 
+        { 
+            return View(); 
+        }
+
+    }
+}
---
+++ buoi1/buoi1/Models/user.cs
@@ -0,0 +1,19 @@
+﻿namespace buoi1.Models
+{
+    public class user
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Email { get; set; }
+        public user() { }
+
+        public user(int id, string name, string email)
+        {
+            Id = id;
+            Name = name;
+            Email = email;
+        }
+
+
+    }
+}

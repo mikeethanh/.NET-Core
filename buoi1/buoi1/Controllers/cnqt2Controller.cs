9a2b878ad58bfd530b6eb472a48b48ae41abbbd4
mikee thanh <mikeethanh04@gmail.com>
2024-09-03 15:02:55 +0700

    add

--- buoi1/buoi1/Controllers/cnqt2Controller.cs
+++ buoi1/buoi1/Controllers/cnqt2Controller.cs
@@ -4,14 +4,14 @@ namespace buoi1.Controllers
 {
     public class cnqt2Controller : Controller
     {
-        public IActionResult Index()
+        public IActionResult Index(string n, int a)
         {
             //de trao doi du lieu tu action ve view co 2 cach 
             //c1: dung cac bo nho chia se nhu: view +bag, viewdata,appdata
             //c2: dung model 
             //vi su cach 1:
-            ViewBag.Name = "Nguyen van a";
-            ViewBag.Age = 22;
+            ViewBag.Name = n;
+            ViewBag.Age = a;
             ViewBag.Score = 4.5;
             //cach2:
             user u = new user(1000, "nmt", "aafasf");
@@ -20,6 +20,16 @@ namespace buoi1.Controllers
         public IActionResult MyAction() 
         { 
             return View(); 
+        }
+
+        public IActionResult GetUser()
+        {
+            return View("PostUser");
+        }
+        [HttpPost]
+        public IActionResult GetUser(user u)
+        {
+            return View("abc",u);
         }
 
     }
